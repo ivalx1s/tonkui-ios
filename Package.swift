b635b8c7ba2bb6e7a6eb90c5b25190048c01cac7
Ivan O <ioparin@pm.me>
2023-04-16 03:06:20 +0400

    updating fonts and pageview modules

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ extension [Package.Dependency] {
 	static var tonkui: [Package.Dependency] {
 		[
 			.package(url: "git@github.com:ivalx1s/tonkui-materials-ios.git", from: "0.1.0"),
-			.package(url: "git@github.com:ivalx1s/tonkui-fonts-ios.git", from: "0.1.1"),
-			.package(url: "git@github.com:ivalx1s/swiftui-pageview.git", from: "0.4.1"),
+			.package(url: "git@github.com:ivalx1s/tonkui-fonts-ios.git", from: "0.1.2"),
+			.package(url: "git@github.com:ivalx1s/swiftui-pageview.git", from: "0.4.2"),
 			.package(url: "git@github.com:ivalx1s/swiftui-plus.git", from: "0.2.0"),
 		]
 	}
